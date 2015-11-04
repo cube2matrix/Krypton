d1d05e3664939c85165c6cf6f5ceca71b3e8d5bd
darrenxyli <darren.xyli@gmail.com>
2015-11-04 16:05:08 -0500

    update script

--- slurm-cse603-spark.sh
+++ slurm-cse603-spark.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-### Make sure that nodes are exclusive for you
-### ntasks-per-node is irrelevant
+####### DO NOT EDIT THIS SECTION
+#SBATCH --clusters=mae
+#SBATCH --partition=planex
+#SBATCH --account=xli66
 #SBATCH --exclusive
-#SBATCH --ntasks-per-node=8
 
-### Customize this section for your job
+####### CUSTOMIZE THIS SECTION FOR YOUR JOB
+####### KEEP --mem=64000 TO USE FULL MEMORY
+#SBATCH --mem=64000
+#SBATCH --job-name="Kryton_darrenxyli_xli66"
 #SBATCH --nodes=4
-#SBATCH --time=1:00:00
-
-#SBATCH --partition=debug
-#SBATCH --job-name="Krypton_darrenxyli_xli66"
+#SBATCH --ntasks-per-node=4
 #SBATCH --output=%j.stdout
 #SBATCH --error=%j.stderr
-#SBATCH --mail-user=xli66@buffalo.edu
-#SBATCH --mail-type=ALL
+#SBATCH --time=01:00:00
+
+# --ntasks-per-node SETS NUMBER OF SPARK EXECUTORS
+# executor_cores SETS NUMBER OF CORES PER EXECUTOR
+executor_cores=4
 
 # MAKE SURE THAT SPARK_LOG_DIR AND SPARK_WORKER_DIR
 # ARE SET IN YOUR BASHRC, FOR EXAMPLE:
 # export SPARK_LOG_DIR=/tmp
-# export SPARK_WORKER_DIR=/scratch/xli66
+# export SPARK_WORKER_DIR=/scratch/your_login
 
-# Add extra modules here
+# ADD EXTRA MODULES HERE IF NEEDED
 
-# Set your command and arguments
+
+# SET YOUR COMMAND AND ARGUMENTS
 PROG="/gpfs/user/xli66/Krypton/Krypton/main.py"
 ARGS="32"
 
@@ -34,7 +39,7 @@ module load hadoop/2.6.0
 module load spark/1.4.1-hadoop
 
 # GET LIST OF NODES
-NODES=(`srun hostname | sort | uniq`)
+NODES=(`srun hostname | sort`)
 
 NUM_NODES=${#NODES[@]}
 LAST=$((NUM_NODES - 1))
@@ -45,18 +50,16 @@ MASTER="spark://${NODES[0]}:7077"
 
 # ALL OTHER NODES ARE WORKERS
 mkdir -p $SLURM_SUBMIT_DIR/$SLURM_JOB_ID
-for i in `seq 1 $LAST`; do
-  ssh ${NODES[$i]} "cd $SPARK_HOME; nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.out" &
+for i in `seq 0 $LAST`; do
+  ssh ${NODES[$i]} "cd $SPARK_HOME; nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.$i.out" &
 done
 
-
 # SUBMIT PYSPARK JOB
-$SPARK_HOME/bin/spark-submit --master $MASTER $PROG $ARGS
-
+$SPARK_HOME/bin/spark-submit --executor-cores $executor_cores --master $MASTER $PROG $ARGS
 
 # CLEAN SPARK JOB
 ssh ${NODES[0]} "cd $SPARK_HOME; ./sbin/stop-master.sh"
 
-for i in `seq 1 $LAST`; do
+for i in `seq 0 $LAST`; do
   ssh ${NODES[$i]} "killall java"
 done
