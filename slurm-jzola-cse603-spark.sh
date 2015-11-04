f0c8d9d92c7f237ce187b8b1f5989af4c76aefc4
darrenxyli <darren.xyli@gmail.com>
2015-11-04 18:09:15 -0500

    script again....

--- slurm-jzola-cse603-spark.sh
+++ slurm-jzola-cse603-spark.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-### Make sure that nodes are exclusive for you
-### ntasks-per-node is irrelevant
-#SBATCH --exclusive
-#SBATCH --ntasks-per-node=8
-
-### Customize this section for your job
-#SBATCH --nodes=4
-#SBATCH --time=1:00:00
-
-#SBATCH --mem=64000
+####### DO NOT EDIT THIS SECTION
 #SBATCH --partition=debug
-#SBATCH --job-name="Krypton_darrenxyli_xli66"
+#SBATCH --exclusive
+
+####### CUSTOMIZE THIS SECTION FOR YOUR JOB
+####### KEEP --mem=64000 TO USE FULL MEMORY
+#######SBATCH --mem=64000
+#SBATCH --job-name="Krypton"
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --output=%j.stdout
 #SBATCH --error=%j.stderr
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=xli66@buffalo.edu
 #SBATCH --mail-type=ALL
 
@@ -26,10 +25,10 @@ executor_cores=4
 # export SPARK_LOG_DIR=/tmp
 # export SPARK_WORKER_DIR=/scratch/xli66
 
-# Add extra modules here
+# ADD EXTRA MODULES HERE IF NEEDED
 module load python/anaconda
 
-# Set your command and arguments
+# SET YOUR COMMAND AND ARGUMENTS
 PROG="/gpfs/user/xli66/Krypton/Krypton/main.py"
 ARGS="32"
 
@@ -40,7 +39,7 @@ module load hadoop/2.6.0
 module load spark/1.4.1-hadoop
 
 # GET LIST OF NODES
-NODES=(`srun hostname | sort | uniq`)
+NODES=(`srun hostname | sort`)
 
 NUM_NODES=${#NODES[@]}
 LAST=$((NUM_NODES - 1))
@@ -51,18 +50,16 @@ MASTER="spark://${NODES[0]}:7077"
 
 # ALL OTHER NODES ARE WORKERS
 mkdir -p $SLURM_SUBMIT_DIR/$SLURM_JOB_ID
-for i in `seq 1 $LAST`; do
-  ssh ${NODES[$i]} "cd $SPARK_HOME; nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.out" &
+for i in `seq 0 $LAST`; do
+  ssh ${NODES[$i]} "cd $SPARK_HOME; nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.$i.out" &
 done
 
-
 # SUBMIT PYSPARK JOB
-$SPARK_HOME/bin/spark-submit --master $MASTER $PROG $ARGS
-
+$SPARK_HOME/bin/spark-submit --executor-cores $executor_cores --master $MASTER $PROG $ARGS
 
 # CLEAN SPARK JOB
 ssh ${NODES[0]} "cd $SPARK_HOME; ./sbin/stop-master.sh"
 
-for i in `seq 1 $LAST`; do
+for i in `seq 0 $LAST`; do
   ssh ${NODES[$i]} "killall java"
 done
