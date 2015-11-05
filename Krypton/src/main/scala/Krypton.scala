2f4a7356e3648ca597b4d7ab9dc0365f8d40eb8b
darrenxyli <darren.xyli@gmail.com>
2015-11-05 13:34:29 -0500

    BetweennessCentrality done

--- Krypton/src/main/scala/Krypton.scala
+++ Krypton/src/main/scala/Krypton.scala
@@ -1,4 +1,4 @@
-import com.darrenxyli.krypton.libs.ShortestPaths
+import com.darrenxyli.krypton.libs.{BetweennessCentrality, ShortestPaths}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx._
 
@@ -7,15 +7,19 @@ object Krypton {
         val logFile = "/Volumes/extend1/amazon/data/pubmed/603/edges/part-00000"
         val conf = new SparkConf().setAppName("Krypton")
         val sc = new SparkContext(conf)
-//        val graph = GraphLoader.edgeListFile(sc, logFile)
-        val edgeSeq = Seq((1, 2), (1, 5), (2, 3), (2, 5), (3, 4), (4, 5), (4, 6)).flatMap {
-            case e => Seq(e, e.swap)
-        }
-        val edges = sc.parallelize(edgeSeq).map { case (v1, v2) => (v1.toLong, v2.toLong) }
-        val graph = Graph.fromEdgeTuples(edges, 1)
+        val graph = GraphLoader.edgeListFile(sc, logFile)
+//        val edgeSeq = Seq((1, 2), (1, 5), (2, 3), (2, 5), (3, 4), (4, 5), (4, 6)).flatMap {
+//            case e => Seq(e, e.swap)
+//        }
+//        val edges = sc.parallelize(edgeSeq).map { case (v1, v2) => (v1.toLong, v2.toLong) }
+//        val graph = Graph.fromEdgeTuples(edges, 1)
         val allSPs = ShortestPaths.run(graph).vertices.collect.flatMap {
             case (v, spMap) => spMap.map {case (k, p) => (k, p+k)}.values
         }
+
+        val allBCValue = BetweennessCentrality.run(graph, allSPs)
+
+        println(allBCValue)
     }
 }
 
