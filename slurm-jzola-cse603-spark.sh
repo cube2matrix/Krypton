e8b855c69cafdc2fec6b3043e3d70d67dd4f40b5
darrenxyli <darren.xyli@gmail.com>
2015-11-05 12:07:50 -0500

    solve the environment problem in script

--- slurm-jzola-cse603-spark.sh
+++ slurm-jzola-cse603-spark.sh
@@ -9,7 +9,7 @@
 #######SBATCH --mem=64000
 #SBATCH --job-name="Krypton"
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=8
 #SBATCH --output=%j.stdout
 #SBATCH --error=%j.stderr
 #SBATCH --time=01:00:00
@@ -18,12 +18,14 @@
 
 # --ntasks-per-node SETS NUMBER OF SPARK EXECUTORS
 # executor_cores SETS NUMBER OF CORES PER EXECUTOR
-executor_cores=4
+executor_cores=8
 
 # MAKE SURE THAT SPARK_LOG_DIR AND SPARK_WORKER_DIR
 # ARE SET IN YOUR BASHRC, FOR EXAMPLE:
-# export SPARK_LOG_DIR=/tmp
-# export SPARK_WORKER_DIR=/scratch/xli66
+export SPARK_LOG_DIR=/tmp/xli66/log
+export SPARK_WORKER_DIR=/tmp/xli66/work
+export SPARK_LOCAL_DIRS=$SPARK_WORKER_DIR
+mkdir -p $SPARK_LOG_DIR $SPARK_WORKER_DIR
 
 # ADD EXTRA MODULES HERE IF NEEDED
 module load python/anaconda
@@ -51,7 +53,7 @@ MASTER="spark://${NODES[0]}:7077"
 # ALL OTHER NODES ARE WORKERS
 mkdir -p $SLURM_SUBMIT_DIR/$SLURM_JOB_ID
 for i in `seq 0 $LAST`; do
-  ssh ${NODES[$i]} "cd $SPARK_HOME; nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.$i.out" &
+  ssh ${NODES[$i]} "cd $SPARK_HOME; module load python/anaconda; nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.$i.out" &
 done
 
 # SUBMIT PYSPARK JOB
