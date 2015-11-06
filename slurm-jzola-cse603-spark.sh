fe22a7e0140ae0569e920af0946e7dc7e09376c5
darrenxyli <darren.xyli@gmail.com>
2015-11-05 22:07:41 -0500

    success running on CCR, bravo

--- slurm-jzola-cse603-spark.sh
+++ slurm-jzola-cse603-spark.sh
@@ -57,7 +57,7 @@ for i in `seq 0 $LAST`; do
 done
 
 # SUBMIT PYSPARK JOB
-$SPARK_HOME/bin/spark-submit --executor-cores $executor_cores --master $MASTER $PROG $ARGS
+$SPARK_HOME/bin/spark-submit --executor-cores $executor_cores --py-files //gpfs//scratch//xli66//nltk.zip --master $MASTER $PROG $ARGS
 
 # CLEAN SPARK JOB
 ssh ${NODES[0]} "cd $SPARK_HOME; ./sbin/stop-master.sh"
