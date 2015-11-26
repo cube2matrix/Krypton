a5b78a3bcbecb6b69119666728fc7dfa6c9cf515
darrenxyli <darren.xyli@gmail.com>
2015-11-25 23:12:23 -0500

    pregel

--- Krypton/src/main/scala/Krypton.scala
+++ Krypton/src/main/scala/Krypton.scala
@@ -1,25 +1,26 @@
-import com.darrenxyli.krypton.libs.{BetweennessCentrality, ShortestPaths}
+import com.darrenxyli.krypton.libs.{BetweennessCentrality, ShortestPaths, BaderBC}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx._
 import scala.compat.Platform.currentTime
 
 object Krypton {
     def main(args: Array[String]) {
-        val logFile = "/gpfs/courses/cse603/students/xli66/603/graph/17lines/edges/part-00000"
+        // val logFile = "/gpfs/courses/cse603/students/xli66/603/graph/17lines/edges/part-00000"
+        val logFile = "/Users/darrenxyli/Documents/3904lines/edges/part-00000"
         val conf = new SparkConf().setAppName("Krypton")
         val sc = new SparkContext(conf)
-        val graph = GraphLoader.edgeListFile(sc, logFile)
+        val graph = GraphLoader.edgeListFile(sc, logFile).cache()
        // val edgeSeq = Seq((1, 2), (1, 5), (2, 3), (2, 5), (3, 4), (4, 5), (4, 6)).flatMap {
        //     case e => Seq(e, e.swap)
        // }
        // val edges = sc.parallelize(edgeSeq).map { case (v1, v2) => (v1.toLong, v2.toLong) }
        // val graph = Graph.fromEdgeTuples(edges, 1)
         val executionStart: Long = currentTime
-        val allSPs = ShortestPaths.run(graph).vertices.collect.flatMap {
-            case (v, spMap) => spMap.map {case (k, p) => (k, p+k)}.values
-        }
-
-        val allBCValue = BetweennessCentrality.run(graph, allSPs)
+        // val allSPs = ShortestPaths.run(graph).vertices.collect.flatMap {
+        //     case (v, spMap) => spMap.map {case (k, p) => (k, p+k)}.values
+        // }
+        // val allBCValue = BetweennessCentrality.run(graph, allSPs)
+        val allSPs = BaderBC.run(graph).vertices.collect()
         val total = currentTime - executionStart
         println("[total " + total + "ms]")
     }
--- Krypton/src/main/scala/com/darrenxyli/krypton/libs/BaderBC.scala
+++ Krypton/src/main/scala/com/darrenxyli/krypton/libs/BaderBC.scala
@@ -6,18 +6,45 @@ import org.apache.spark.graphx._
 import scala.reflect.ClassTag
 
 object BaderBC {
+  /** Stores a map from the vertex id of a landmark to the distance to that landmark. */
+  type SPMap = Map[VertexId, Int]
 
-    private def calculateSingleNode[VD: ClassTag](node : graphx.VertexId, VD): Unit = {
+  private def makeMap(x: (VertexId, Int)*) = Map(x: _*)
 
+  private def incrementMap(spmap: SPMap): SPMap = spmap.map { case (v, d) => v -> (d + 1) }
+
+  private def addMaps(spmap1: SPMap, spmap2: SPMap): SPMap =
+    (spmap1.keySet ++ spmap2.keySet).map {
+      k => k -> math.min(spmap1.getOrElse(k, Int.MaxValue), spmap2.getOrElse(k, Int.MaxValue))
+    }.toMap
+
+  /**
+   * Computes shortest paths to the given set of landmark vertices.
+   *
+   * @tparam ED the edge attribute type (not used in the computation)
+   *
+   * @param graph the graph for which to compute the shortest paths
+   * @param landmarks the list of landmark vertex ids. Shortest paths will be computed to each
+   * landmark.
+   *
+   * @return a graph where each vertex attribute is a map containing the shortest-path distance to
+   * each reachable landmark vertex.
+   */
+  def run[VD, ED: ClassTag](graph: Graph[VD, ED]): Graph[SPMap, ED] = {
+    val spGraph = graph.mapVertices { (vid, attr) => makeMap(vid -> 0) }
+
+    val initialMessage = makeMap()
+
+    def vertexProgram(id: VertexId, attr: SPMap, msg: SPMap): SPMap = {
+      addMaps(attr, msg)
     }
 
-
-    def run[VD, ED: ClassTag](graph: Graph[VD, ED]): BCMap = {
-
-        val bcMap = graph.vertices.map { case (vid, attr) => vid -> 0.0 }.collect().toMap
-
-        for s in graph.vertices
-
-
+    def sendMessage(edge: EdgeTriplet[SPMap, _]): Iterator[(VertexId, SPMap)] = {
+      val newAttr = incrementMap(edge.dstAttr)
+      if (edge.srcAttr != addMaps(newAttr, edge.srcAttr)) Iterator((edge.srcId, newAttr))
+      else Iterator.empty
     }
+
+    Pregel(spGraph, initialMessage)(vertexProgram, sendMessage, addMaps)
+  }
 }
