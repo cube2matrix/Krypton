8c6c70ee44ec8b3f3b80ce48366713ae46a26fbf
darrenxyli <darren.xyli@gmail.com>
2015-11-29 18:24:40 -0500

    requeue

--- script/genec/3904/6-3-4-bc.sh
+++ script/genec/3904/6-3-4-bc.sh
@@ -6,19 +6,20 @@
 
 ####### CUSTOMIZE THIS SECTION FOR YOUR JOB
 ####### KEEP --mem=64000 TO USE FULL MEMORY
-#SBATCH --mem=48000
+#SBATCH --mem=80000
 #SBATCH --job-name="Krypton"
-#SBATCH --nodes=3
-#SBATCH --ntasks-per-node=2
+#SBATCH --nodes=6
+#SBATCH --ntasks-per-node=3
 #SBATCH --output=%j.stdout
 #SBATCH --error=%j.stderr
-#SBATCH --time=06:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mail-user=xli66@buffalo.edu
 #SBATCH --mail-type=ALL
+#SBATCH --requeue
 
 # --ntasks-per-node SETS NUMBER OF SPARK EXECUTORS
 # executor_cores SETS NUMBER OF CORES PER EXECUTOR
-executor_cores=6
+executor_cores=4
 
 # MAKE SURE THAT SPARK_LOG_DIR AND SPARK_WORKER_DIR
 # ARE SET IN YOUR BASHRC, FOR EXAMPLE:
@@ -33,7 +34,7 @@ module load python/anaconda
 # SET YOUR COMMAND AND ARGUMENTS
 PROG="/user/xli66/Krypton/Krypton/target/scala-2.10/krypton_2.10-0.1.jar"
 ARGS="/gpfs/courses/cse603/students/xli66/603/graph/3904lines/edges/part-00000"
-PNUM="36"
+PNUM="128"
 
 ####### DO NOT EDIT THIS PART
 module load java/1.8.0_45
@@ -53,11 +54,11 @@ MASTER="spark://${NODES[0]}:7077"
 # ALL OTHER NODES ARE WORKERS
 mkdir -p $SLURM_SUBMIT_DIR/$SLURM_JOB_ID
 for i in `seq 0 $LAST`; do
-  ssh ${NODES[$i]} "cd $SPARK_HOME; nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.$i.out" &
+  ssh ${NODES[$i]} "cd $SPARK_HOME;  nohup ./bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER &> $SLURM_SUBMIT_DIR/$SLURM_JOB_ID/nohup-${NODES[$i]}.$i.out" &
 done
 
 # SUBMIT PYSPARK JOB
-$SPARK_HOME/bin/spark-submit --conf spark.akka.frameSize=512 --conf spark.driver.maxResultSize=5g --num-executors 6 --driver-memory 6g --executor-memory 8g --executor-cores $executor_cores --jars $(echo /user/xli66/Krypton/Krypton/lib/*.jar | tr ' ' ',') --master $MASTER $PROG $ARGS $PNUM
+$SPARK_HOME/bin/spark-submit --conf spark.akka.frameSize=512 --conf spark.storage.memoryFraction=0.3 --conf spark.shuffle.memoryFraction=0.1 --conf spark.default.parallelism=$PNUM --conf spark.driver.maxResultSize=8g --num-executors 17 --driver-memory 8g --executor-memory 6g --executor-cores $executor_cores --jars $(echo /user/xli66/Krypton/Krypton/lib/*.jar | tr ' ' ',') --master $MASTER $PROG $ARGS $PNUM
 
 # CLEAN SPARK JOB
 ssh ${NODES[0]} "cd $SPARK_HOME; ./sbin/stop-master.sh"
